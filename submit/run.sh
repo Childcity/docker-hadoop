56eac66213d3581ce25f5c0d9c2f8319340370be
Your Name <you@example.com>
2020-10-06 20:00:55 +0300

    de

--- submit/run.sh
+++ submit/run.sh
@@ -5,9 +5,11 @@ export HADOOP_CLASSPATH=$($HADOOP_HOME/bin/hadoop classpath)
 
 #rm -rf $APP/com
 
-cd $APP_DIR && \
-javac -classpath ${HADOOP_CLASSPATH} ${APP_MAIN_CLASS}.java -d . && \
-jar -cvf ${APP_MAIN_CLASS}.jar *.class && \
+cd $APP_DIR/k-means && \
+ls -al && \
+javac -sourcepath ./ -classpath ${HADOOP_CLASSPATH} -d ./ ${APP_MAIN_CLASS}.java && \
+ls -al && \
+jar -cf $APP_DIR/${APP_MAIN_CLASS}.jar *.class && \
 $HADOOP_HOME/bin/hadoop jar $JAR_FILEPATH $CLASS_TO_RUN $PARAMS
 
 echo "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
